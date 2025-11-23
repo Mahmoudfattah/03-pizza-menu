4587637a006df0bddf62df3d2a095eb2b58ccaa8
Mahmoud-Fattah <hoodafatah@gmail.com>
2025-11-23 23:25:13 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //section 4 props
-import react from 'react';
+
 import ReactDom from 'react-dom/client';
 import './index.css';
 
@@ -8,42 +8,42 @@ const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "/pizzas/focaccia.jpg",
+    photoName: "./pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "/pizzas/margherita.jpg",
+    photoName: "./pizzas/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "/pizzas/spinaci.jpg",
+    photoName: "./pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "/pizzas/funghi.jpg",
+    photoName: "./pizzas/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "/pizzas/salamino.jpg",
+    photoName: "./pizzas/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "/pizzas/prosciutto.jpg",
+    photoName: "./pizzas/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -137,8 +137,10 @@ function Pizza({pizzaObj}){
 
 function Footer() {
   const hour = new Date().getHours();
+ 
   const openHour = 12;
   const closeHour = 22;
+  
   const isOpen = hour >= openHour && hour <= closeHour;
   
 
